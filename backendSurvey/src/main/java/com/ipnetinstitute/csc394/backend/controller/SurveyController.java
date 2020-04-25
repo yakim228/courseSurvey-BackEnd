7460094f43c05d5b06d76795eb94fae0f28a82ee
mvincentbb <mvincentbb@gmail.com>
2020-04-25 11:19:20 +0000

    ajout de la route /getAllSurveyByStudent/{id} pour obtenir la liste des survey aux quelles a participer un etudiant donne

--- backendSurvey/src/main/java/com/ipnetinstitute/csc394/backend/controller/SurveyController.java
+++ backendSurvey/src/main/java/com/ipnetinstitute/csc394/backend/controller/SurveyController.java
@@ -2,9 +2,31 @@ package com.ipnetinstitute.csc394.backend.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ipnetinstitute.csc394.backend.dao.SurveyEntityRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
 @RestController
 public class SurveyController {
-	
-	
+    
+    @Autowired
+    private SurveyEntityRepository surveyRepo;
+    
+	@GetMapping("/getAllSurveyByStudent/{id}")
+    public List getAllSurveyByStudent(@PathVariable("id") Integer id) {
+
+        List result = new ArrayList();
+        try {
+            result = (List) surveyRepo.findAllSurveyRespondbyUser(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            return result;
+        }
+    }
 
 }
--- backendSurvey/src/main/java/com/ipnetinstitute/csc394/backend/dao/SurveyEntityRepository.java
+++ backendSurvey/src/main/java/com/ipnetinstitute/csc394/backend/dao/SurveyEntityRepository.java
@@ -4,10 +4,20 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
+
+import org.springframework.data.jpa.repository.Query;
+// import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import com.ipnetinstitute.csc394.backend.entity.Survey;
+
+
 @Transactional
 public interface SurveyEntityRepository extends BaseEntityRepository<Survey>  {
-	
+    
+    @Query(value = "select * from survey sv inner join student_survey studs on sv.id = studs.id_survey  inner join student s on studs.id_student = s.id   where s.id = :UserID group by sv.id ", nativeQuery = true)
+    List findAllSurveyRespondbyUser(@Param("UserID") Integer id);
+
+
 
 		
 }
