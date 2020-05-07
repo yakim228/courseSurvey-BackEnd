acd8088dc82ca5f3d307fd3a1101b5f4e8d5b828
Franc Balakim <balakress@gmail.com>
2020-05-07 22:57:31 +0000

    extension de la classse BackendSurveyApplication

--- backendSurvey/src/main/java/com/ipnetinstitute/csc394/backend/BackendSurveyApplication.java
+++ backendSurvey/src/main/java/com/ipnetinstitute/csc394/backend/BackendSurveyApplication.java
@@ -4,9 +4,10 @@ package com.ipnetinstitute.csc394.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BackendSurveyApplication {
+public class BackendSurveyApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendSurveyApplication.class, args);
