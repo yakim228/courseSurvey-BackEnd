a4b0ab51e8c9ac9ead9399fd656fca3b22cbe621
Franc Balakim <balakress@gmail.com>
2020-05-11 13:34:40 +0000

    correction des conflits

--- backendSurvey/src/main/java/com/ipnetinstitute/csc394/backend/BackendSurveyApplication.java
+++ backendSurvey/src/main/java/com/ipnetinstitute/csc394/backend/BackendSurveyApplication.java
@@ -1,11 +1,5 @@
 package com.ipnetinstitute.csc394.backend;
 
-//package com.ipnetinstitute.csc394.backend;
-<<<<<<< HEAD
-=======
-
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
->>>>>>> 79b2814f52cbed17169e2b53a4ed3c5db1c1863c
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
