a018993813cc9a129237f05b12e51faa5fa89a09
Panawe Batanado <panawe.batanado@ipnetinstitute.com>
2020-05-17 22:08:13 -0400

    Change

--- backendSurvey/src/test/java/com/example/demo/BackendSurveyApplicationTests.java
+++ backendSurvey/src/test/java/com/example/demo/BackendSurveyApplicationTests.java
@@ -54,45 +54,40 @@ class BackendSurveyApplicationTests extends SpringBootServletInitializer{
 
 		this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).apply(documentationConfiguration(restDocumentation)).build();
 	}
-        @Test
-	@Order(1)
-	public void signupTest() throws JsonProcessingException, Exception {
-		System.out.println("SignupTest is called");
-		User user = new User("John", "Doe", "+228 90 12 13 54", "john.doe@gmail.com");
-		user.setModBy(1);
-		user.setUserName("john.doe");
-		user.setPassword("secret@Password");
-		user.setModDateTime(new Date());
-		user.setCreateDateTime(new Date());
-		System.out.println(user);
-		String userJson = mapper.writeValueAsString(user);
-		System.out.println(userJson);
-		try {
-			mockMvc.perform(post("/api/auth/signup").contentType(MediaType.APPLICATION_JSON).content(userJson))
-					.andExpect(status().isOk())
-                                        .andExpect(jsonPath("userName", is(user.getUserName())))
-					.andDo(document("signup", preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint()),
-							requestFields(fieldWithPath("id").description("User id").ignored(),
-									fieldWithPath("userName").description("Nom d'utilisateur"),
-									fieldWithPath("password").description("Mot de passe"),
-									fieldWithPath("firstName").description("Prenom"),
-									fieldWithPath("lastName").description("Nom"),
-									fieldWithPath("phone").description("Telephone"),
-									fieldWithPath("email").description("E-mail"),
-                                                                        fieldWithPath("student").description("La liste d'eleves"),
-                                                                        fieldWithPath("role").description("La liste de roles"),
-                                                                        fieldWithPath("teacher").description("La liste d'enseignants"),
-									fieldWithPath("type").description("Set this to user"),
-									fieldWithPath("createDateTime").description("Date de creation").ignored(),
-									fieldWithPath("modDateTime").description("Date de modification").ignored(),
-									fieldWithPath("error").description("Utiliser pour le message d'erreur").ignored(),
-									fieldWithPath("modBy").description("Id de la personne connetee"))));
-
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw (e);
-		}
-	}
+	/*
+	 * @Test
+	 * 
+	 * @Order(1) public void signupTest() throws JsonProcessingException, Exception
+	 * { System.out.println("SignupTest is called"); User user = new User("John",
+	 * "Doe", "+228 90 12 13 54", "john.doe@gmail.com"); user.setModBy(1);
+	 * user.setUserName("john.doe"); user.setPassword("secret@Password");
+	 * user.setModDateTime(new Date()); user.setCreateDateTime(new Date());
+	 * System.out.println(user); String userJson = mapper.writeValueAsString(user);
+	 * System.out.println(userJson); try {
+	 * mockMvc.perform(post("/api/auth/signup").contentType(MediaType.
+	 * APPLICATION_JSON).content(userJson)) .andExpect(status().isOk())
+	 * .andExpect(jsonPath("userName", is(user.getUserName())))
+	 * .andDo(document("signup", preprocessRequest(prettyPrint()),
+	 * preprocessResponse(prettyPrint()),
+	 * requestFields(fieldWithPath("id").description("User id").ignored(),
+	 * fieldWithPath("userName").description("Nom d'utilisateur"),
+	 * fieldWithPath("password").description("Mot de passe"),
+	 * fieldWithPath("firstName").description("Prenom"),
+	 * fieldWithPath("lastName").description("Nom"),
+	 * fieldWithPath("phone").description("Telephone"),
+	 * fieldWithPath("email").description("E-mail"),
+	 * fieldWithPath("student").description("La liste d'eleves"),
+	 * fieldWithPath("role").description("La liste de roles"),
+	 * fieldWithPath("teacher").description("La liste d'enseignants"),
+	 * fieldWithPath("type").description("Set this to user"),
+	 * fieldWithPath("createDateTime").description("Date de creation").ignored(),
+	 * fieldWithPath("modDateTime").description("Date de modification").ignored(),
+	 * fieldWithPath("error").description("Utiliser pour le message d'erreur").
+	 * ignored(),
+	 * fieldWithPath("modBy").description("Id de la personne connetee"))));
+	 * 
+	 * } catch (Exception e) { e.printStackTrace(); throw (e); } }
+	 */
  /*       
         @Test
 	@Order(2)
