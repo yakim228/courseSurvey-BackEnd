4b558b84766c594f8ac811ff4fa3468b35e66ff2
Panawe Batanado <panawe.batanado@ipnetinstitute.com>
2020-05-23 11:22:05 -0400

    Le survey doit retourner le un JSON

--- backendSurvey/src/main/java/com/ipnetinstitute/csc394/backend/dao/QuestionEntityRepository.java
+++ backendSurvey/src/main/java/com/ipnetinstitute/csc394/backend/dao/QuestionEntityRepository.java
@@ -18,7 +18,7 @@ public interface QuestionEntityRepository extends BaseEntityRepository<Question>
    
 
     @Query(value = "select  q.* from  question q inner join cat_survey_question  csq on q.id =csq.id_question inner join cat_survey cs on csq.id_cat_survey = cs.id  where csq.id_cat_survey = :CategoryID ", nativeQuery = true)
-    List findAllQuestionbyCategory(@Param("CategoryID") Integer id);
+    List<Question> findAllQuestionbyCategory(@Param("CategoryID") Integer id);
 
     @Query(value = "SELECT Q.* FROM survey S INNER JOIN cat_survey CS ON (S.ID_CAT_SURVEY = CS.ID) "
             + "INNER JOIN cat_survey_question CSQ " +
@@ -27,6 +27,6 @@ public interface QuestionEntityRepository extends BaseEntityRepository<Question>
 
             "WHERE S.ID=:surveyID AND CSQ.ID_QUESTION NOT IN (SELECT SS.ID_QUESTION FROM student_survey SS, "
             + "student ST WHERE ST.id = SS.id_student AND  ST.id_user = :userID)", nativeQuery = true)
-    List  pendingSurveyQuestions(@Param("surveyID") Integer id_survey, @Param("userID") Integer id_user);
+    List<Question>  pendingSurveyQuestions(@Param("surveyID") Integer id_survey, @Param("userID") Integer id_user);
 
 }
--- backendSurvey/src/main/java/com/ipnetinstitute/csc394/backend/dao/SurveyEntityRepository.java
+++ backendSurvey/src/main/java/com/ipnetinstitute/csc394/backend/dao/SurveyEntityRepository.java
@@ -17,32 +17,32 @@ public interface SurveyEntityRepository extends BaseEntityRepository<Survey>  {
     @Query(value = "select sv.* from survey sv inner join student_survey " +
      "studs on sv.id = studs.id_survey  inner join student s " +
       "on studs.id_student = s.id   where s.id = :userId  ", nativeQuery = true)
-    List findAllSurveyRespondbyUser(@Param("userId") Integer userId);
+    List<Survey> findAllSurveyRespondbyUser(@Param("userId") Integer userId);
 
 
     @Query(value = "SELECT * FROM survey s2 WHERE s2.end_date >= now()", nativeQuery = true)
-    List findAllSurveyStillValid();
+    List<Survey> findAllSurveyStillValid();
 
 
     @Query(value = "select * from survey sur " +
     		"where sur.id_cat_survey = :categoryId " +
     		 "and sur.id_course = :courseId",
     		 nativeQuery = true)
-    List findbyCategoryAndCourse(@Param("categoryId") Integer catSurveyId , @Param("courseId") Integer idCourse);
+    List<Survey> findbyCategoryAndCourse(@Param("categoryId") Integer catSurveyId , @Param("courseId") Integer idCourse);
 
     
     @Query(value= "SELECT DISTINCT S.* FROM survey S INNER JOIN cat_survey CS ON (S.id_cat_survey = CS.id)" + 
             " INNER JOIN cat_survey_question CSQ ON (CSQ.id_cat_survey= CS.id) " +
             "WHERE CSQ.id_question NOT IN (SELECT SS.id_question " +
              "FROM student_survey SS, student ST WHERE ST.id = SS.id_student AND  ST.id_user = :UserID);", nativeQuery = true)
-    List pendingSurvey(@Param("UserID") Integer userId);
+    List<Survey> pendingSurvey(@Param("UserID") Integer userId);
 
     
     @Query(value= "select sy.* from survey sy " +
     			"inner  join course c on sy.id_course = c.id "  +
     			"where c.id_teacher =:teacherId",
     			nativeQuery = true)
-    List SurveyByTeacher(@Param("teacherId") Integer teacherId);
+    List<Survey> SurveyByTeacher(@Param("teacherId") Integer teacherId);
 
     
 
