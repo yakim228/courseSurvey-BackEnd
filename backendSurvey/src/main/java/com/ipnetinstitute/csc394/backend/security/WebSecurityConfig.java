0bb9341d77ba449472dfa094dd46bef12dc6581a
Franc Balakim <balakress@gmail.com>
2020-06-05 00:39:15 +0000

    parametrages des fiches statics

--- backendSurvey/src/main/java/com/ipnetinstitute/csc394/backend/security/WebSecurityConfig.java
+++ backendSurvey/src/main/java/com/ipnetinstitute/csc394/backend/security/WebSecurityConfig.java
@@ -61,6 +61,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/api/auth/**").permitAll()
+                .antMatchers("/**", ".html").permitAll()
                 .antMatchers("/").permitAll()
                 .anyRequest().authenticated();
         
